1cb6b8d78345574d231eb2ece86bb9db03156cdf
sacamiso <sacamiso@unirioja.es>
2023-03-08 19:46:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/racetracker/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.racetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.racetracker.ui.RaceTrackerApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    RaceTrackerApp()
+                }
+            }
+        }
+    }
+}
